fb4fc2f0d6cdbd3126431618113ef11ddf1d21a1
Adam Gordon <adamjgordon24@gmail.com>
2019-02-21 00:21:07 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,17 @@
-# code here!
+require "pry" 
+
+class School
+  
+  def initialize(name)
+    @name = name
+  end 
+  
+ @roster = {}
+ def roster
+   @roster
+ end
+ binding.pry
+ def add_student 
+ end
+ 
+end
